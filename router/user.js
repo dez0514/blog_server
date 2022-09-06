1f15f87ddb4c9e097acf5daba32789e005986824
dez0514 <2573433803@qq.com>
2022-09-06 22:26:25 +0800

    token create

--- router/user.js
+++ router/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 // const sqlTool = require('../utils/handle')
 const query = require('../utils/pool_async')
 const json = require('../utils/response')
+const jwt = require('jsonwebtoken')
 
 // 注册
 router.post('/register',async function (req, res) {
@@ -26,23 +27,25 @@ router.post('/register',async function (req, res) {
   }
 });
 // 登录
-router.get('/login', async function (req, res) {
+router.post('/login', async function (req, res) {
   const params = req.body;
   try {
     const { username, password } = params
     const sqlQuery = `SELECT username, password FROM users WHERE username=?;`
     const data = await query(sqlQuery, username)
-    console.log(data)
-    if(!data) {
+    console.log(data[0])
+    if(!data || !data[0]) {
       json(res, 1, null, '用户不存在!')
       return
     }
-    if(!data || !data.username || data.password !== password) {
+    if(!data[0].username || data[0].password !== password) {
       json(res, 1, null, '用户名或密码不正确!')
       return
     }
     // todo: jwt
-    json(res, 0, data, '登录成功!')
+    // 登陆成功生成 token 返回给客户端 第一个参数 是 组 ，第二个是 私钥
+    const token = jwt.sign({ username }, 'zwdisagoodboy')
+    json(res, 0, { token }, '登录成功!')
   } catch(err) {
     json(res, 1, err, '登录失败!')
   }
